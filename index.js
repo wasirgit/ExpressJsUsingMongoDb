12d83b406b40600e3992d52296c6143f42c2f45b
Asieuzzaman Wasir <wasir.cse@gmail.com>
2024-02-22 14:04:31 +0600

    implemented CRUD operation

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const todohandler = require("./routeHandler/todoHandler");
-// initiate express app
+const todoHandler = require("./routeHandler/todoHandler");
+
+// express app initialization
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
-// connect to database
-
+// database connection with mongoose
 mongoose
-  .connect("mongodb://localhost:27017/todos")
-  .then(() => {
-    console.log("Connection Successfull");
-  })
-  .catch((err) => {
-    console.log(`Error ${err}`);
-  });
+  .connect("mongodb://localhost/todos")
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.log(err));
 
-// application route
+// application routes
+app.use("/todo", todoHandler);
 
-app.use("/todo", todohandler);
-
-// default error handling
+// default error handler
 function errorHandler(err, req, res, next) {
-  if (res.headerSent) {
+  if (res.headersSent) {
     return next(err);
   }
   res.status(500).json({ error: err });
 }
 
 app.listen(3000, () => {
-  console.log("app is listening to 3000 port");
+  console.log("app listening at port 3000");
 });
--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,18 +1,92 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
 
+const Todo = new mongoose.model("Todo", todoSchema);
+console.log("Enter to todo handler");
 // get all todos
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (err) {
+    res.status(500).json({ error: "There was a server side error" });
+  }
+});
 // get a todos by ID
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+  try {
+    const todos = await Todo.findById(id);
+    if (!todos) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.status(200).json(todos);
+  } catch (err) {
+    res.status(500).json({ error: "There was a server side error" });
+  }
+});
 
-//post a todo
-router.post("/", async (req, res) => {});
+// POST A TODO
+router.post("/", async (req, res) => {
+  console.log("saving todo into databse");
+  const newTodo = new Todo(req.body);
+  try {
+    await newTodo.save();
+    res.status(200).json({ message: "Todo was inserted successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: "There was a server side error!" });
+  }
+});
 //post multiple todos
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+  console.log("saving multiples todos into databse");
+  try {
+    await Todo.insertMany(req.body);
+    res.status(200).json({ message: "Todos were inserted successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: "There was a server side error!" });
+  }
+});
 // Put a todo
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  console.log(`updating todo ${req.params.id}`);
+  const { id } = req.params;
+  const update = req.body;
+
+  try {
+    // findByIdAndUpdate method
+    const updatedTodo = await Todo.findByIdAndUpdate(id, update, {
+      new: true, // Return the updated document
+      // Optionally, you can use other options like: upsert, runValidators, etc.
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    res.status(500).json({ error: "There was a server side error" });
+  }
+});
 // delete a todo
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  console.log(`updating todo ${req.params.id}`);
+  const { id } = req.params;
+
+  try {
+    const deleteTodo = await Todo.findByIdAndDelete(id);
+    if (!deleteTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.status(200).json({ message: "Todo deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting todo:", err);
+    res.status(500).json({ error: "There was a server side error" });
+  }
+});
 
 module.exports = router;
